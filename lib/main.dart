7581726c4e471dce0d2dfba76c2ca4f467b35ef3
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-01-20 23:23:54 -0500

    added: add or remove favourite words

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,11 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPair = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
 
   Widget _buildList() {
     return ListView.builder(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, item) {
           if (item.isOdd) return Divider();
           final index = item ~/ 2;
@@ -35,9 +36,20 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
     return ListTile(
-      title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18),),
-    );
+        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18)),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _savedWordPairs.remove(pair);
+            } else {
+              _savedWordPairs.add(pair);
+            }
+          });
+        });
   }
 
   Widget build(BuildContext context) {
